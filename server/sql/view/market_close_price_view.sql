b9bdb6a5e07dbc5c9bfcc79f803d897db54652d2
shinji5761 <shinji.fsi.5761@gmail.com>
2017-06-24 14:45:54 +0900

    【Server】 1日足の終値の修正

--- server/sql/view/market_close_price_view.sql
+++ server/sql/view/market_close_price_view.sql
@@ -1,7 +1,6 @@
 -- 市場情報 高値､安値､出来高
 DROP VIEW IF EXISTS bid_manager.market_close_price;
 CREATE VIEW bid_manager.market_close_price AS (
-	SELECT brand_code, target_date, MAX(hour) AS hour, MAX(min) AS min, close FROM market_info
-	GROUP BY brand_code, target_date
-	HAVING hour = MAX(hour) AND min = MAX(min)
+	SELECT brand_code, target_date, market_price, date_time FROM market_info_time
+	WHERE date_time in (SELECT MAX(date_time) FROM market_info_time GROUP BY brand_code, target_date)
 );
--- server/sql/view/market_one_day_info_view.sql
+++ server/sql/view/market_one_day_info_view.sql
@@ -1,7 +1,7 @@
 -- 市場情報 1日足
 DROP VIEW IF EXISTS bid_manager.market_one_day_info;
 CREATE VIEW bid_manager.market_one_day_info AS (
-	SELECT open.brand_code, open.target_date, open, high, low, close, volume
+	SELECT open.brand_code, open.target_date, open, high, low, market_price, volume
 	FROM market_open_price AS open
 	INNER JOIN market_close_price AS close
 	ON open.target_date = close.target_date
