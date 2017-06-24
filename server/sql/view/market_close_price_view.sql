7a5d1d027a89810137406ab4124bcda5bb6cff47
shinji5761 <shinji.fsi.5761@gmail.com>
2017-06-24 14:49:45 +0900

    終値の属性名の修正

--- server/sql/view/market_close_price_view.sql
+++ server/sql/view/market_close_price_view.sql
@@ -1,6 +1,6 @@
 -- 市場情報 高値､安値､出来高
 DROP VIEW IF EXISTS bid_manager.market_close_price;
 CREATE VIEW bid_manager.market_close_price AS (
-	SELECT brand_code, target_date, market_price, date_time FROM market_info_time
+	SELECT brand_code, target_date, market_price AS close, date_time FROM market_info_time
 	WHERE date_time in (SELECT MAX(date_time) FROM market_info_time GROUP BY brand_code, target_date)
 );
